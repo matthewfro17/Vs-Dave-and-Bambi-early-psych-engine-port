d9d8ef742223f586d3e6fa81c3d84736d79db804
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-02-07 17:05:08 -0500

    Add files via upload

---
+++ example_mods/stages/banana-hell.lua
@@ -0,0 +1,23 @@
+--created with Super_Hugo's Stage Editor v1.6.3
+
+function onCreate()
+
+	makeLuaSprite('obj1', 'bananaVoid1', -854, -484)
+    addGlitchEffect('obj1', 2, 2)
+	setObjectOrder('obj1', 1)
+	scaleObject('obj1', 1.5, 1.5)
+	addLuaSprite('obj1', true)
+
+	makeLuaSprite('obj2', 'bananaVoid2', -854, -484)
+    addGlitchEffect('obj2', 2, 2)
+	setObjectOrder('obj2', 1)
+	scaleObject('obj2', 1.5, 1.5)
+	addLuaSprite('obj2', true)
+
+	makeLuaSprite('obj3', 'bananaVoid3', -854, -484)    
+    addGlitchEffect('obj3', 2, 2)
+	setObjectOrder('obj3', 1)
+	scaleObject('obj3', 1.5, 1.5)
+	addLuaSprite('obj3', true)
+
+end
