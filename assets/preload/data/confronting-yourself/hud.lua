1a73165edd004ca5e89c58f4d8539a78ed91acff
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-24 19:17:54 -0400

    Add files via upload

---
+++ assets/preload/data/confronting-yourself/hud.lua
@@ -0,0 +1,15 @@
+function onCreate()
+
+    local diff = getProperty('storyDifficultyText')
+
+    makeLuaText('songText', songName.. '', 0,3,678);
+    addLuaText('songText',true);
+	setTextSize('songText',14)
+    setTextBorder("songText", 1, '000000')
+
+    makeLuaText('message','Song by Cuzsie!', 0,3,695);
+    addLuaText('message',true);
+	setTextSize('message',14)
+    setTextBorder("message", 1, '000000')
+
+end
